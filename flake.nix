78a99ce8c51283ce896e06e24ab96a109752e406
Pedro Markun <pedro@markun.com.br>
2026-07-30 19:35:51 -0300

    ci: pin and lint github actions workflow

--- flake.nix
+++ flake.nix
@@ -19,6 +19,7 @@
         {
           default = pkgs.mkShell {
             packages = with pkgs; [
+              actionlint
               cargo
               clippy
               docker-client
